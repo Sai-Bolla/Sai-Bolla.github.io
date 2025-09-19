fa0e613683a5393ee12f2cbe2933b4bbc2163cad
Sai-Bolla <74682258+Sai-Bolla@users.noreply.github.com>
2025-09-19 23:26:18 +0100

    add dark mode
    add dark mode and improved footer

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function hideSidebar(){
     const sideBar = document.querySelector('.sidebar')
     sideBar.style.display = 'none';
 }
+
+function themeToggle(){
+    var theme = document.body;
+    theme.classList.toggle("darkMode");
+}
+
+function underConstruction(){
+    alert(" Under Construction! \n 🧱🚧🏗️👷👷‍♀️🧑‍🏭🛠️🔩⚒️🔨")
+}
